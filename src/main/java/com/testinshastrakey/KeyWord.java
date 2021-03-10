cbcd4dc7ebcea1c80d6164640711a84eccd15220
shitalPachore12 <shitalpachore022@gmail.com>
2021-03-10 15:14:10 +0530

    Updating pom.xml and Keyword class

---
+++ src/main/java/com/testinshastrakey/KeyWord.java
@@ -0,0 +1,16 @@
+package com.testinshastrakey;
+
+public class KeyWord {
+
+	public void openBrowser(String browser) {
+		switch (browser) {
+		case "Chrome" :
+			break;
+		case "Firefox":
+			break;
+		case "IE"
+		break;
+		default:
+		}
+	}
+}
